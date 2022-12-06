97ccb33374c226a0a2890c493704d11fa492e839
Prd46 <prd46@drexel.edu>
2022-12-06 14:45:24 -0500

    Tweaks

--- final/build/scripts/modalApp.js
+++ final/build/scripts/modalApp.js
@@ -16,15 +16,30 @@ button.addEventListener('mouseout', function (event){
     if (gClickDetected == false){
     button.classList.remove('interactionBox__active');
     }
+    if (gClickDetected == true){
+        button.classList.remove('interactionBox__squeeze');
+    button.classList.remove('interactionBox__active');
+    button.classList.add('interactionBox__clicked');
+        body.classList.add('preventScroll');
+        setTimeout(function(){
+        modal.classList.add('interactionModal__active');
+        exit.classList.remove('modalX__fadeOut');
+            setTimeout(function(){
+                gClickDetected = false;
+                button.classList.remove('interactionBox__internal');
+                button.classList.remove('interactionBox__clicked');
+                }, 800);
+        }, 600);
+        }
 });
 
 button.addEventListener('mousedown', function (event){
+    gClickDetected = true;
     button.classList.add('interactionBox__squeeze');
     button.classList.add('interactionBox__internal');
 });
 
 button.addEventListener('mouseup', function (event){
-    gClickDetected = true;
     button.classList.remove('interactionBox__squeeze');
     button.classList.remove('interactionBox__active');
     button.classList.add('interactionBox__clicked');
